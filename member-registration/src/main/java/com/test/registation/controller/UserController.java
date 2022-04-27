2e5ada5c226e368a6eea1299eb1c39fadc6fcd91
Napat <srifahnapat@gmail.com>
2022-04-27 19:43:09 +0700

    v-4 add user service

--- member-registration/src/main/java/com/test/registation/controller/UserController.java
+++ member-registration/src/main/java/com/test/registation/controller/UserController.java
@@ -1,2 +1,95 @@
-package com.test.registation.controller;public class UserController {
+package com.test.registation.controller;
+
+import com.test.registation.entity.User;
+import com.test.registation.exception.BusinessException;
+import com.test.registation.model.CommonResponse;
+import com.test.registation.model.UserProfile;
+import com.test.registation.repository.UserRepository;
+import com.test.registation.service.UserService;
+import com.test.registation.utils.CommonResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import static com.test.registation.config.JwtAuthTokenFilter.username;
+import static com.test.registation.constant.BusinessCode.RGTE1000;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserService userService;
+
+    Logger logger = Logger.getLogger(UserController.class.getName());
+
+    @GetMapping("/list")
+    public ResponseEntity<CommonResponse<List<UserProfile>>> postRegisterProfile() {
+        CommonResponse<List<UserProfile>> response;
+        try{
+            response = userService.getUserList();
+        }catch (BusinessException be){
+            return ResponseEntity.badRequest()
+                    .body(CommonResponseUtil.createResponse(be.getCode(), be.getMessage()));
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{reference-code}")
+    public ResponseEntity<CommonResponse<UserProfile>> getUserByReferenceCode(
+            @PathVariable("reference-code") @NotEmpty String referenceCode
+    ) {
+        CommonResponse<UserProfile> response;
+        try{
+            response = userService.getUserByReferenceCode(referenceCode);
+        }catch (BusinessException be){
+            return ResponseEntity.badRequest()
+                    .body(CommonResponseUtil.createResponse(be.getCode(), be.getMessage()));
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<CommonResponse<User>> updateUserProfile(
+            HttpServletRequest request,
+            @RequestBody UserProfile param
+    ) {
+        logger.info(username);
+        Optional<User> user = userRepository.findByReferenceCode(param.getReferenceCode());
+        CommonResponse<User> response = null;
+        try{
+            if(user.isPresent()){
+            if(username.equals(user.get().getUsername())){
+                try{
+                    response = userService.updateUserProfile(param);
+                }catch (BusinessException be){
+                    return ResponseEntity.badRequest()
+                            .body(CommonResponseUtil.createResponse(be.getCode(), be.getMessage()));
+                }
+            }else {
+                throw new BusinessException(RGTE1000);
+            }
+        }else {
+                throw new BusinessException(RGTE1000);
+        }
+        }catch (BusinessException be){
+            return ResponseEntity.badRequest()
+                    .body(CommonResponseUtil.createResponse(be.getCode(), be.getMessage()));
+        }
+
+        return ResponseEntity.ok(response);
+    }
 }
--- member-registration/src/main/java/com/test/registation/service/UserService.java
+++ member-registration/src/main/java/com/test/registation/service/UserService.java
@@ -1,2 +1,99 @@
-package com.test.registation.service;public class UserService {
+package com.test.registation.service;
+
+import com.test.registation.constant.StatusCode;
+import com.test.registation.entity.User;
+import com.test.registation.exception.BusinessException;
+import com.test.registation.model.CommonResponse;
+import com.test.registation.model.UserProfile;
+import com.test.registation.repository.UserRepository;
+import com.test.registation.utils.CommonResponseUtil;
+import com.test.registation.utils.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.test.registation.constant.BusinessCode.*;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public CommonResponse<UserProfile> getUserByReferenceCode( String referenceCode){
+        UserProfile userProfile = new UserProfile();
+        try{
+            Optional<User> dbUser = userRepository.findByReferenceCode(referenceCode);
+            if(!dbUser.isPresent()){
+                throw new BusinessException(RGTE1004);
+            }else {
+                    userProfile.setName(dbUser.get().getName());
+                    userProfile.setEmail(dbUser.get().getEmail());
+                    userProfile.setPhoneNumber(dbUser.get().getPhoneNumber());
+                    userProfile.setSalary(dbUser.get().getSalary());
+                    userProfile.setMemberType(dbUser.get().getMemberType());
+            }
+        }catch (BusinessException be){
+            throw be;
+        } catch (Exception e){
+            throw new BusinessException(USDB1000);
+        }
+        return CommonResponseUtil.createResponse(StatusCode.S0000,userProfile);
+    }
+
+    public CommonResponse<List<UserProfile>> getUserList(){
+        List<UserProfile> userProfileList = new ArrayList<>();
+        try{
+            List<User> userList = userRepository.findAll();
+            if(userList.isEmpty()){
+                throw new BusinessException(RGTE1004);
+            }else {
+                for (User user: userList) {
+                    UserProfile userProfile = new UserProfile();
+                    userProfile.setReferenceCode(user.getReferenceCode());
+                    userProfile.setName(user.getName());
+                    userProfile.setEmail(user.getEmail());
+                    userProfile.setPhoneNumber(user.getPhoneNumber());
+                    userProfile.setSalary(user.getSalary());
+                    userProfile.setMemberType(user.getMemberType());
+                    userProfileList.add(userProfile);
+                }
+            }
+        }catch (BusinessException be){
+            throw be;
+        } catch (Exception e){
+            throw new BusinessException(USDB1000);
+        }
+        return CommonResponseUtil.createResponse(StatusCode.S0000,userProfileList);
+    }
+
+    public CommonResponse<User> updateUserProfile (UserProfile param){
+        Optional<User> dbUser;
+        try{
+            dbUser =  userRepository.findByReferenceCode(param.getReferenceCode());
+            if(!dbUser.isPresent()){
+                throw new BusinessException(RGTE1004);
+            }else {
+                User updatedUserProfile = dbUser.get();
+                if(param.getName()!=null){
+                    updatedUserProfile.setName(param.getName());
+                }
+                if(param.getPhoneNumber()!=null){
+                    updatedUserProfile.setPhoneNumber(param.getPhoneNumber());
+                }
+                if(param.getSalary()!=null){
+                    updatedUserProfile.setSalary(param.getSalary());
+                    updatedUserProfile.setMemberType(CommonUtil.setMemberType(param.getSalary()));
+                }
+                userRepository.save(updatedUserProfile);
+            }
+        }catch (BusinessException be){
+            throw be;
+        } catch (Exception e){
+            throw new BusinessException(USDB1000);
+        }
+        return CommonResponseUtil.createResponse(StatusCode.S0000,dbUser.get());
+    }
 }
